0b99daf041f0558f9cc79bbd3a16add460a7610a
Chris Autwell <cautwell@gmail.com>
2023-01-11 08:48:14 -0600

    Drop comments

--- game.rb
+++ game.rb
@@ -71,14 +71,14 @@ class Grid
        (Window.viewport_height / TILE_SIZE).times do |y|
          cell_is_alive = cells.has_key?([x, y])
          cells_friends_count = [
-           cells.has_key?([x-1, y-1]), # Top Left
-           cells.has_key?([x  , y-1]), # Top
-           cells.has_key?([x+1, y-1]), # Top Right
-           cells.has_key?([x+1, y  ]), # Right
-           cells.has_key?([x+1, y+1]), # Bottom Right
-           cells.has_key?([x  , y+1]), # Bottom
-           cells.has_key?([x-1, y+1]), # Bottom left
-           cells.has_key?([x-1, y  ]), # left
+           cells.has_key?([x-1, y-1]),
+           cells.has_key?([x  , y-1]),
+           cells.has_key?([x+1, y-1]),
+           cells.has_key?([x+1, y  ]),
+           cells.has_key?([x+1, y+1]),
+           cells.has_key?([x  , y+1]),
+           cells.has_key?([x-1, y+1]),
+           cells.has_key?([x-1, y  ]),
          ].count(true)
 
          if((cell_is_alive && cells_friends_count.between?(2,3)) || (!cell_is_alive && cells_friends_count == 3))
