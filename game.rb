e160ca000fba7f2ca4482a7bf8248226bf81aed4
Chris Autwell <cautwell@gmail.com>
2023-01-11 09:11:23 -0600

    Lint

--- game.rb
+++ game.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # Game of Life
 # Click on a square to create a square
 # Press p to play
@@ -6,19 +8,20 @@
 
 require 'ruby2d'
 require_relative 'grid'
+
 TILE_SIZE = 25
 GRID_SIZE = 10
 LINE_COLOR = 'lime'
 BACKGROUND_COLOR = 'black'
 SQUARE_COLOR = 'lime'
 
-set title: "Game of Life",
-  background: 'black',
-  resizable: false,
-  borderless: false,
-  viewport_width: TILE_SIZE * GRID_SIZE,
-  viewport_height: TILE_SIZE * GRID_SIZE,
-  height: Window.height + TILE_SIZE
+set title: 'Game of Life',
+    background: 'black',
+    resizable: false,
+    borderless: false,
+    viewport_width: TILE_SIZE * GRID_SIZE,
+    viewport_height: TILE_SIZE * GRID_SIZE,
+    height: Window.height + TILE_SIZE
 
 grid = Grid.new
 
@@ -29,16 +32,12 @@ on :mouse_down do |event|
 end
 
 on :key_down do |event|
-  if event.key == 'p'
-    grid.toggle_status
-  end
+  grid.toggle_status if event.key == 'p'
   if event.key == 'c'
     grid.started = false
     grid.reset
   end
-  if event.key == 'escape'
-    exit
-  end
+  exit if event.key == 'escape'
 end
 
 update do
@@ -46,9 +45,7 @@ update do
   grid.draw_vertical_lines
   grid.draw_horizontal_lines
   grid.populate_cells
-  if Window.frames % 10 == 0
-    grid.move_cells
-  end
+  grid.move_cells if (Window.frames % 10).zero?
 end
 
 show
--- grid.rb
+++ grid.rb
@@ -1,13 +1,15 @@
+# frozen_string_literal: true
 
 class Grid
   attr_accessor :cells, :started
+
   def initialize
-    @cells = Hash.new
+    @cells = {}
     @started = false
   end
 
   def reset
-    @cells = Hash.new
+    @cells = {}
   end
 
   def draw_horizontal_lines
@@ -25,10 +27,10 @@ class Grid
   def toggle_cell(x, y)
     cell_x = x * TILE_SIZE
     cell_y = y * TILE_SIZE
-    if cells.has_key?([x, y])
-      cells.delete([x,y])
+    if cells.key?([x, y])
+      cells.delete([x, y])
     else
-      cells[[x , y]] = true
+      cells[[x, y]] = true
     end
   end
 
@@ -37,35 +39,35 @@ class Grid
   end
 
   def populate_cells
-    cells.keys.each do |x, y|
+    cells.each_key do |x, y|
       Square.new(x: x * TILE_SIZE, y: y * TILE_SIZE, size: TILE_SIZE, color: SQUARE_COLOR)
     end
   end
 
   def move_cells
-    if started
-      new_cells = Hash.new
+    return unless started
 
-      (Window.viewport_width / TILE_SIZE ).times do |x|
-       (Window.viewport_height / TILE_SIZE).times do |y|
-         cell_is_alive = cells.has_key?([x, y])
-         cells_friends_count = [
-           cells.has_key?([x-1, y-1]),
-           cells.has_key?([x  , y-1]),
-           cells.has_key?([x+1, y-1]),
-           cells.has_key?([x+1, y  ]),
-           cells.has_key?([x+1, y+1]),
-           cells.has_key?([x  , y+1]),
-           cells.has_key?([x-1, y+1]),
-           cells.has_key?([x-1, y  ]),
-         ].count(true)
+    new_cells = {}
 
-         if((cell_is_alive && cells_friends_count.between?(2,3)) || (!cell_is_alive && cells_friends_count == 3))
-           new_cells[[x, y]] = true
-         end
-       end
+    (Window.viewport_width / TILE_SIZE).times do |x|
+      (Window.viewport_height / TILE_SIZE).times do |y|
+        cell_is_alive = cells.key?([x, y])
+        cells_friends_count = [
+          cells.key?([x - 1, y - 1]),
+          cells.key?([x, y - 1]),
+          cells.key?([x + 1, y - 1]),
+          cells.key?([x + 1, y]),
+          cells.key?([x + 1, y + 1]),
+          cells.key?([x, y + 1]),
+          cells.key?([x - 1, y + 1]),
+          cells.key?([x - 1, y])
+        ].count(true)
+
+        if (cell_is_alive && cells_friends_count.between?(2, 3)) || (!cell_is_alive && cells_friends_count == 3)
+          new_cells[[x, y]] = true
+        end
       end
-      @cells = new_cells
     end
+    @cells = new_cells
   end
 end
